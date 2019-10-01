39463086de028ab25c89226c3e765555ec2c6f40
remi.odufuye <remi.odufuye@gmail.com>
2019-10-01 18:30:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  
   var animal = myAnimal('cat') ;
+  
   return animal ; 
 }
 
